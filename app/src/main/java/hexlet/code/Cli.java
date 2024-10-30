1f7cf9f771866a2780b77705ae78c719a724dafd
root <root@WIN-U5ESP2I9R8V.localdomain>
2024-10-30 16:54:01 +0300

    Fourth

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-	public static void sayHello() {
-		System.out.println("Welcome to the Brain Games!");
-		System.out.println("May I have your name?");
-		Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
-		System.out.println("Hello, " + name + "!");
-	}
+    public static void sayHello() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
 }
